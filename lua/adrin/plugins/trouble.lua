c923cbf408ec2b4357df242c76efd5d98124939a
Adrin Amin Salehi <adrin.amin@gmail.com>
2024-06-16 08:50:11 +0200

    Adding default opts to trouble

--- lua/adrin/plugins/trouble.lua
+++ lua/adrin/plugins/trouble.lua
@@ -1,6 +1,7 @@
 return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
     keys = {
         { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
