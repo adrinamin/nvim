a5918ed169f9af73b7852559d1ddedf429c256e7
Adrin Amin Salehi <adrinamin@fedora-personal.lan>
2024-06-26 17:15:22 +0200

    Update plugins

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
-vim.env.LANGUAGE = 'en_US.UTF-8'
+vim.env.LANGUAGE = "en_US.UTF-8"
+vim.opt.termguicolors = true -- this is useful when using tmux
 
-require('adrin.core')
-require('adrin.lazy')
-
-
+require("adrin.core")
+require("adrin.lazy")
