40685df3ccb4649c0f7d8b065151cfdcd44096d8
Adrin Amin Salehi <adrin.amin@gmail.com>
2024-07-05 21:22:33 +0200

    Adding github copilot

--- lua/adrin/plugins/nvim-cmp.lua
+++ lua/adrin/plugins/nvim-cmp.lua
@@ -1,3 +1,11 @@
+local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+    end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+
 return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -22,6 +30,13 @@ return {
 
         local lspkind = require("lspkind")
 
+        lspkind.init({
+            symbol_map = {
+                Copilot = "",
+            },
+        })
+        vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+
         -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
         require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -42,6 +57,13 @@ return {
                 ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
                 ["<C-e>"] = cmp.mapping.abort(), -- close completion window
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                ["<Tab>"] = vim.schedule_wrap(function(fallback)
+                    if cmp.visible() and has_words_before() then
+                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                    else
+                        fallback()
+                    end
+                end),
             }),
             -- sources for autocompletion
             sources = cmp.config.sources({
@@ -49,6 +71,7 @@ return {
                 { name = "luasnip" }, -- snippets
                 { name = "buffer" }, -- text within current buffer
                 { name = "path" }, -- file system paths
+                { name = "copilot" }, -- github copilot
             }),
 
             -- configure lspkind for vs-code like pictograms in completion menu
@@ -57,6 +80,23 @@ return {
                     maxwidth = 50,
                     ellipsis_char = "...",
                 }),
+            },
+
+            -- configure sorting
+            sorting = {
+                priority_weight = 2,
+                comparators = {
+                    require("copilot_cmp.comparators").prioritize,
+                    -- Below is the default comparitor list and order for nvim-cmp
+                    cmp.config.compare.offset,
+                    -- cmp.config.compare.scopes, -- this is the commented in nvim-cmp too
+                    cmp.config.compare.exact,
+                    cmp.config.compare.score,
+                    cmp.config.compare.kind,
+                    cmp.config.compare.sort_text,
+                    cmp.config.compare.length,
+                    cmp.config.compare.order,
+                },
             },
         })
     end,
