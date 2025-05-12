60e0d12423303b4374859c1ca54b5c29faff061b
Adrin Amin Salehi <adrin.amin@gmail.com>
2025-05-12 17:27:08 +0200

    Fix global vim undefined warnings

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
-vim.env.LANGUAGE = 'en_US.UTF-8'
+-- Add the vim module to the global environment
+package.loaded.vim = vim
+_G.vim = vim
+vim.env.LANGUAGE = "en_US.UTF-8"
 
-require('adrin.core')
-require('adrin.lazy')
-
-
+require("adrin.core")
+require("adrin.lazy")
