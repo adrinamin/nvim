d2be1992e83a786b54fca2e9c58c1774c3acb08c
Adrin Amin Salehi <adrin.amin@gmail.com>
2025-05-13 19:48:08 +0200

    Adding vim module to the global environment

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- Add the vim module to the global environment
+package.loaded.vim = vim
+_G.vim = vim
+
 vim.env.LANGUAGE = "en_US.UTF-8"
 vim.opt.termguicolors = true -- this is useful when using tmux
 
