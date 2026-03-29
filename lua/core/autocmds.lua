851adcbe93b2ce099e7218b731ee082edf4fa191
Adrin Amin Salehi <adrin.amin@protonmail.com>
2026-03-29 12:52:16 +0200

    Update lsp autocomplete

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -15,15 +15,25 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
-		local id = vim.tbl_get(ev, "data", "client_id")
-		local client = id and vim.lsp.get_client_by_id(id)
-		if client == nil then
-			return
-		end
-
-		if client:supports_method("textDocument/completion") then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
+		-- old version
+		-- local id = vim.tbl_get(ev, "data", "client_id")
+		-- local client = id and vim.lsp.get_client_by_id(id)
+		-- if client == nil then
+		-- 	return
+		-- end
+		--
+		-- if client:supports_method("textDocument/completion") then
+		-- 	vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		-- end
+		-- new version
+		vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, {
+			autotrigger = true,
+			convert = function(item)
+				-- Optional: Format items (e.g., truncate labels)
+				return { abbr = item.label, menu = item.detail }
+			end,
+		})
+		vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
 	end,
 })
 
