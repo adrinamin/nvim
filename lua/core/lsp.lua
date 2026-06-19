2e1e9bde848f5cdf3bee1a59b6ca9d5cd8d8da56
Adrin Amin Salehi <adrin.amin@protonmail.com>
2026-06-19 13:47:15 +0200

    Change diagnostic behavior

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,8 +1,8 @@
 vim.lsp.enable({ "lua_ls", "bicep", "azure_pipeline_ls", "bashls" }) -- "bicep"
 
 vim.diagnostic.config({
-	virtual_lines = true,
-	-- virtual_text = true,
+	virtual_lines = false,
+	virtual_text = false,
 	underline = true,
 	update_in_insert = false,
 	severity_sort = true,
@@ -11,12 +11,12 @@ vim.diagnostic.config({
 		source = true,
 	},
 	signs = {
-		-- text = {
-		--     [vim.diagnostic.severity.ERROR] = "󰅚 ",
-		--     [vim.diagnostic.severity.WARN] = "󰀪 ",
-		--     [vim.diagnostic.severity.INFO] = "󰋽 ",
-		--     [vim.diagnostic.severity.HINT] = "󰌶 ",
-		-- },
+		text = {
+			[vim.diagnostic.severity.ERROR] = "E",
+			[vim.diagnostic.severity.WARN] = "W",
+			[vim.diagnostic.severity.INFO] = "I",
+			[vim.diagnostic.severity.HINT] = "H",
+		},
 		numhl = {
 			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
 			[vim.diagnostic.severity.WARN] = "WarningMsg",
