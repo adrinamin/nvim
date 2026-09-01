4c390ef26b543c9d4ce5c63b3bf73367cf8f51a5
Adrin Amin Salehi <adrin.amin@protonmail.com>
2026-09-01 14:32:46 +0200

    Change tab style

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -47,4 +47,28 @@ vim.o.pumheight = 7
 
 set.shortmess:append("c")
 
+-- tabs
 vim.o.showtabline = 2
+
+-- Source - https://stackoverflow.com/a/79344770
+-- Posted by Carson
+-- Retrieved 2026-04-14, License - CC BY-SA 4.0
+
+function _G.get_tabline()
+	local s = ""
+	for tabnr = 1, vim.fn.tabpagenr("$") do
+		local winnr = vim.fn.tabpagewinnr(tabnr)
+		local buflist = vim.fn.tabpagebuflist(tabnr)[winnr]
+		local bufname = vim.fn.bufname(buflist)
+		local bufname_short = vim.fn.fnamemodify(bufname, ":t")
+		if tabnr == vim.fn.tabpagenr() then
+			s = s .. "%#TabLineSel#" .. " " .. tabnr .. ": " .. bufname_short .. " "
+		else
+			s = s .. "%#TabLine#" .. " " .. tabnr .. ": " .. bufname_short .. " "
+		end
+	end
+	s = s .. "%#TabLineFill#"
+	return s
+end
+
+vim.o.tabline = "%!v:lua.get_tabline()"
